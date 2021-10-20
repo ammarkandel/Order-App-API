ab527c42d6b0b526d4feeec74201586f12e1690b
ammarkandel <merom8703@gmail.com>
2021-10-20 21:17:41 +0200

    Handle syntax in testing files

--- spec/requests/order_request_spec.rb
+++ spec/requests/order_request_spec.rb
@@ -10,10 +10,6 @@ RSpec.describe 'Orders', type: :request do
 
     before { get "/api/v1/users/#{user_id}/orders.json" }
 
-    it 'Expect not empty response data' do
-      expect(JSON.parse(response.body)).not_to be_empty
-    end
-
     it 'Return 200 response' do
       expect(response).to have_http_status(:success)
     end
--- spec/requests/user_request_spec.rb
+++ spec/requests/user_request_spec.rb
@@ -2,7 +2,16 @@ require 'rails_helper'
 
 RSpec.describe 'Users', type: :request do
   before do
-    post '/api/v1/signup.json', params: { user: { first_name: 'Ammar', middle_name: 'El Refay', last_name: 'Kandeel', email: 'merom8703@gmail.com', phone_number: '01011637887', password: '12345678' } }
+    def user_info
+      { first_name: 'Ammar',
+        middle_name: 'El Refay',
+        last_name: 'Kandeel',
+        email: 'merom8703@gmail.com',
+        phone_number: '01011637887',
+        password: '12345678' }
+    end
+
+    post '/api/v1/signup.json', params: { user: user_info }
   end
 
   describe 'POST create user' do
@@ -30,8 +39,7 @@ RSpec.describe 'Users', type: :request do
     end
 
     it 'Expect to get the token' do
-      expect(JSON.parse(response.body)['jwt'])
-      p "The JWT Token ( #{JSON.parse(response.body)['jwt']} )"
+      expect(JSON.parse(response.body)['jwt']).not_to eq(nil)
     end
   end
 end
